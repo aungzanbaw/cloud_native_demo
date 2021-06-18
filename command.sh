f8677290be736050f5030a8c50ec994ca5b815fb
Aung Zan Baw <aungzanbaw@gmail.com>
2021-06-18 08:00:17 +0630

    Add all essential commands for ref

--- command.sh
+++ command.sh
@@ -1,20 +1,51 @@
 Exe 3.8
     Essential 
         # build the image
-        docker build -t go-helloworld .
+        docker build -t {NAME} .
 
         # tag the image
-        docker tag go-helloworld pixelpotato/go-helloworld:v1.0.0
+        docker tag {NAME} {DOCKER-HUB-NAME}/{TAG-NAME}
 
         # push the image
-        docker push pixelpotato/go-helloworld:v1.0.0
+        docker push {DOCKER-HUB-NAME}/{TAG-NAME}
+    Useful
+        # get all images 
+        docker images
+
+        # get processes status
+        docker ps
+Exe 3.15
+    Essential 
+        # get control plan or add-ons endpoints 
+        kubectl cluster-info
+
+        # get all the nodes in a cluster
+        kubectl get nodes 
+
+        # get all the information/logs
+        kubectl cluster-info dump
+
+        # get all the configurations/details about the node
+        kubectl describe node {NODE-NAME}
+
 
 Exe 3.16
     Essential 
-        kubectl create deploy go-helloworld --image=aungbaw/go-helloworld:v1.0.0
+        # create new deploy using given image
+        kubectl create deploy {NAME} --image={DOCKER-HUB-NAME}/{TAG-NAME}
+        
+        # run the image, it's automatically started
         kubtctl run 
-        kubectl get po
-        kubectl get rs
-        kubectl get deploy
+        
     Useful 
-        kubectl 
+        # get all pods
+        kubectl get po
+
+        # get replica set
+        kubectl get rs
+
+        # get updates for pods and rs
+        kubectl get deploy # or deployments 
+
+        # delete deploy
+        kubectl delete deploy {name}
